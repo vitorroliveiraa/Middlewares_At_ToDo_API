5999ba8d246200be0fcf6e59cc60c16e7806a4d4
Vitor Oliveira <vitorroliveiraa@outlook.com.br>
2021-07-22 17:46:36 -0300

    Falta corrigir erros

--- src/index.js
+++ src/index.js
@@ -10,19 +10,67 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+
+  const userExists = users.find((user) => user.username === username);
+
+  if (!userExists) {
+    return response.status(404).json({ error: 'Username not found!' });
+  }
+
+  request.user = userExists;
+
+  return next();
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request;
+
+  const todoLength = user.todos.length;
+
+  if (user.pro === false && todoLength >= 10) {
+    return response.status(403);
+  }
+
+  return next();
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers;
+  const { idTodo } = request.params;
+
+  const userExists = users.find((user) => user.username === username);
+  const todoValid = userExists.todos.find((todo) => todo.id === idTodo);
+  const isUUID = validate(idTodo);
+
+  if (!userExists) {
+    return response.status(404);
+  }
+
+  if (!isUUID) {
+    return response.status(400);
+  }
+
+  if (!todoValid) {
+    return response.status(404);
+  }
+
+  request.user = userExists;
+  request.todo = todoValid;
+  return next();
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params;
+
+  const user = users.find((user) => user.id === id);
+
+  if (!user) {
+    return response.status(404).json({ error: 'User not found!' });
+  }
+  request.user = user;
+
+  return next();
 }
 
 app.post('/users', (request, response) => {
